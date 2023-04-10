f3f23b688c74acf64d2ddd6a5b15dcfa277fe976
ljshwyykl <ljs@gmail.com>
2023-04-10 20:21:58 +0800

    Add web3 authentication and increase test coverage
    - Add web3 authentication endpoints
    - Update test command to use experimental VM modules
    
    [service/test/app.spec.ts]
    - Add `ethers` import
    - Add `dotenv` import and configuration
    - Add `POST /api/web3/challenge` endpoint
    - Add `POST /api/web3/login` endpoint with signature verification
    [service/package.json]
    - Change the test command to use the experimental VM modules

--- service/test/app.spec.ts
+++ service/test/app.spec.ts
@@ -1,9 +1,48 @@
 import request from 'supertest'
+import { ethers } from 'ethers';
 import app from '../src/app'
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 describe('GET /healthcheck', () => {
 	it('should return a 200 status code', async () => {
 		const response = await request(app).get('/healthcheck');
+		expect(response.status).toBe(200);
+	})
+})
+
+
+describe('POST /api/web3/challenge', () => {
+	it('should return a 200 status code', async () => {
+		const response = await request(app).post('/api/web3/challenge')
+			.send({ "address": "0xD3420A3be0a1EFc0FBD13e87141c97B2C9AC9dD3" });
+		console.log("response.body", response.body);
+		expect(response.status).toBe(200);
+	})
+})
+
+describe('POST /api/web3/login', () => {
+	it('should return a 200 status code', async () => {
+
+		const response = await request(app).post('/api/web3/challenge')
+			.send({ "address": "0xD3420A3be0a1EFc0FBD13e87141c97B2C9AC9dD3" });
+
+		// Replace with your own private key
+		const privateKey = process.env.PRIVATE_KEY
+
+		// Create a new instance of the ethers Wallet class
+		const wallet = new ethers.Wallet(privateKey)
+
+
+		// Sign the message using the wallet
+		const signature = await wallet.signMessage(response.body.message)
+
+		console.log(signature)
+
+
+		const response1 = await request(app).post('/api/web3/login')
+			.send({ "address": "0xD3420A3be0a1EFc0FBD13e87141c97B2C9AC9dD3", signature });
+		console.log("response1.body", response1.body);
 		expect(response.status).toBe(200);
 	})
 })
